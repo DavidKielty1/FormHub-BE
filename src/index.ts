43da7c4b308750e5a79de1b2c84afa01c4ab8ea2
David Kielty <davidkielty@live.co.uk>
2024-01-22 13:25:16 +0000

    Docker compose and prisma/knex db migration

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
 import express from 'express';
 import morgan from 'morgan';
 import db from './modules/db';
+import { nanoid } from 'nanoid';
+export const genId = () => nanoid(16);
+
+// const seedDatabase = async () => {
+//   if ((await db.submission.count()) === 0) {
+//     await db.submission.createMany({
+//       data: [
+//         {
+//           id: genId(),
+//           submittedAt: new Date(),
+//           data: {
+//             name: 'Kevin Wade',
+//             twitter: 'kevinwwwade',
+//           },
+//         },
+//       ],
+//     });
+//   }
+// };
+// seedDatabase();
 
 const app = express();
 app.use(morgan('dev')); // logger
 
 app.get('/', async (req, res) => {
-  const posts = await db.post.findMany();
-  res.json(posts);
+  const submissions = await db.submission.findMany();
+  res.json(submissions);
 });
 
 const port = Number(process.env.PORT ?? 8080);
